615fa067281e59e793805cd696d5cf1d4085e033
liaopen123 <467969@qq.com>
2020-07-01 10:49:15 +0800

    头部框架的抽取  include 公共art

--- js/nodejs/express/bolg_web/app.js
+++ js/nodejs/express/bolg_web/app.js
@@ -6,6 +6,14 @@ const app = express();
 
 //开放所有的静态文件
 let staticFile = path.join(__dirname, 'public');
+
+
+//关联art-template 模板
+app.engine('art', require('express-art-template'));
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'art');
+
+
 app.use(express.static(staticFile));
 app.use('', home);
 app.use('/admin', admin);
--- js/nodejs/express/bolg_web/router/admin.js
+++ js/nodejs/express/bolg_web/router/admin.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const admin = express.Router();
 admin.get('', (req, res) => {
-    res.send('admin');
+    res.send('end');
+});
+admin.get('/login', (req, res) => {
+    res.render('./admin/login');
 });
 
 module.exports = admin;
